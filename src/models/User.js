0158622f6ce15ba901392299863160b16886d19d
Minh Ho <khacminhrt@gmail.com>
2022-08-11 12:34:33 +0700

    add trip

--- src/models/User.js
+++ src/models/User.js
@@ -4,10 +4,18 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
   name: String,
+  firstName: String,
+  lastName: String,
+
   username: { type: String, required: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['Driver', 'Customer', 'Admin'], required: true },
-  isActive: Boolean,
+
+  status: {
+    type: 'String',
+    enum: ['Active', 'Pending', 'Deactivated'],
+  },
+  // isActive: Boolean,
 });
 
 const User = mongoose.model('user', userSchema);
